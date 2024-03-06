4473f501d4d87167875ffb4a3a82b9d9bed1e104
ryescholin <ryescholin@gmail.com>
2024-03-05 16:02:59 -0800

    blink

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,19 @@
-use gpio::{GpioOut};
 use std::thread;
-use std::time;
+use std::time::Duration;
+use rppal::gpio::{Gpio, OutputPin};
 
-const PIN_SIGNAL_LIGHT: u64 = 21;
-
-struct SignalLight {
-    gpio_pin: gpio::sysfs::SysFsGpioOutput,
-}
-
-impl SignalLight {
-    fn new() -> Result<Self, gpio::Error> {
-        let gpio_pin = gpio::sysfs::SysFsGpioOutput::open(PIN_SIGNAL_LIGHT)?;
-        Ok(Self { gpio_pin })
-    }
-
-    fn disable(&mut self) -> Result<(), gpio::Error> {
-        self.gpio_pin.set_value(false)?;
-        Ok(())
-    }
-
-    fn enable(&mut self) -> Result<(), gpio::Error> {
-        self.gpio_pin.set_value(true)?;
-        Ok(())
-    }
-}
-
-impl Drop for SignalLight {
-    fn drop(&mut self) {
-        if let Err(e) = self.disable() {
-            eprintln!("Error cleaning up GPIO pin: {:?}", e);
-        }
-    }
-}
+const GPIO_PIN: u8 = 21;
 
 fn main() {
-    let mut signal_light = match SignalLight::new() {
-        Ok(light) => light,
-        Err(e) => {
-            eprintln!("Error initializing SignalLight: {:?}", e);
-            return;
-        }
-    };
+    // Initialize the GPIO
+    let gpio = Gpio::new().unwrap();
+    let mut pin = gpio.get(GPIO_PIN).unwrap().into_output();
 
-    // Enable the signal light
-    if let Err(e) = signal_light.enable() {
-        eprintln!("Error enabling signal light: {:?}", e);
-        return;
+    // Blink the LED
+    loop {
+        pin.set_high();
+        thread::sleep(Duration::from_millis(500)); // Wait for 0.5 seconds
+        pin.set_low();
+        thread::sleep(Duration::from_millis(500)); // Wait for 0.5 seconds
     }
-
-    // Sleep for a while to demonstrate
-    thread::sleep(time::Duration::from_secs(5));
 }
